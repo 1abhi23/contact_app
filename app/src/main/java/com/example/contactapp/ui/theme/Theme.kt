24468bacfa375aebdf893ba0dc0fe407e51b753f
Abhishek Jasrotia <https://github.com/1abhi23>
2025-08-17 12:30:04 +0530

    changed primary color to Blue

--- app/src/main/java/com/example/contactapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/contactapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.contactapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,13 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = GreenJC,
+    primary = BlueJC,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = GreenJC,
+    primary = BlueJC,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
